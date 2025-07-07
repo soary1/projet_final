9cd74dd99720a222d317aa838eda453ecc26b1cc
Noah <141527201+NoahRDN@users.noreply.github.com>
2025-07-08 01:27:56 +0300

    pret retarde par type

--- ws/models/TypePret.php
+++ ws/models/TypePret.php
@@ -1,23 +1,29 @@
 <?php
 require_once __DIR__ . '/../db.php';
 
-class TypePret {
-    public static function all(): array {
-        return getDB()->query("SELECT * FROM banque_type_pret")
-                      ->fetchAll(PDO::FETCH_ASSOC);
+class TypePret
+{
+    public static function all(): array
+    {
+        return getDB()->query("SELECT * FROM banque_type_pret")->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function create(string $libelle, float $taux): int {
-        $st = getDB()->prepare(
-            "INSERT INTO banque_type_pret (libelle,taux) VALUES (?,?)"
-        );
-        $st->execute([$libelle, $taux]);
-        return (int) getDB()->lastInsertId();
+    public static function getById(int $id): ?array
+    {
+        $st = getDB()->prepare("SELECT * FROM banque_type_pret WHERE id = ?");
+        $st->execute([$id]);
+        return $st->fetch(PDO::FETCH_ASSOC) ?: null;
     }
-    public static function getById($id) {
-    $st = getDB()->prepare("SELECT * FROM banque_type_pret WHERE id = ?");
-    $st->execute([$id]);
-    return $st->fetch(PDO::FETCH_ASSOC);
-}
+
+    public static function create(string $nom, float $taux, int $duree, int $delai_defaut): int
+    {
+        $db = getDB(); 
+        $st = $db->prepare("
+            INSERT INTO banque_type_pret (nom, taux_interet, duree_mois, delai_defaut)
+            VALUES (?, ?, ?, ?)
+        ");
+        $st->execute([$nom, $taux, $duree, $delai_defaut]);
+        return (int) $db->lastInsertId(); 
+    }
 
 }

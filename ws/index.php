056a0b1847c7b8ff210e81d909231d4ef4217aff
Noah <141527201+NoahRDN@users.noreply.github.com>
2025-07-07 18:48:31 +0300

    integration fond

--- ws/index.php
+++ ws/index.php
@@ -1,7 +1,6 @@
 <?php
 require 'vendor/autoload.php';
 require 'db.php';
-require 'routes/PretRoutes.php';
 require 'routes/etudiant_routes.php';
 require 'routes/client_routes.php';
 require 'routes/pres_routes.php';
